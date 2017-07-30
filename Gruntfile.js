caaacce9eeaf379cd7b6ddfd56153530fd91eadc
Antonio Pardo <apardo.socket@gmail.com>
2017-07-30 18:44:57 +0200

    no compress files

--- Gruntfile.js
+++ Gruntfile.js
@@ -255,7 +255,7 @@ module.exports = function (grunt) {
       main: {
         options:{
           mangle: false,
-          compress: {},
+          compress: false,
           beautify: true
         },
         files: {
